e5272dc66a16eeafae56b830ef3a22d7e52ae5a5
vikram-singh9 <163000375+vikram-singh9@users.noreply.github.com>
2025-02-21 00:57:19 +0500

    Update app.tsx

--- app/components/app.tsx
+++ app/components/app.tsx
@@ -1,7 +1,6 @@
 "use client";
 import React, { useEffect, useRef, useState } from "react";
 import { ForwardIcon, Upload, Pause, PlayIcon, RewindIcon } from "lucide-react";
-import Image from "next/image";
 
 interface Track {
   title: string;
